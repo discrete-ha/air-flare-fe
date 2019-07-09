f94c55d02c444e90304cc9bdc488dd9eeae1a9b7
John Ha <discreteb@gmail.com>
2019-07-09 15:21:27 +0900

    fc

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.get('*', function(req, res){
+    res.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+    console.log("server is running")
+});
