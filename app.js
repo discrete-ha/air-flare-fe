37959de72b4b3f8b4901678d8381565c228c390d
discrete-ha <discreteb@gmail.com>
2019-07-09 17:39:40 +0900

    add start

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+const host = '0.0.0.0';
+const port = process.env.PORT || 5000;
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', port);
 app.use(express.static(__dirname + '/'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
@@ -17,6 +19,6 @@ app.get('*', function(req, res){
     res.render('index.html');
 });
 
-app.listen(app.get('port'), function() {
-    console.log("server is running...")
+app.listen(port, host, function() {
+  console.log("Server started.......");
 });
